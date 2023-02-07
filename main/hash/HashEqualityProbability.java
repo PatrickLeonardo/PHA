5ac75788ea67de4cca80331c9e73f13f76d8ef7d
PatrickLeonardo <ticklf7@gmail.com>
2023-02-06 22:39:25 -0300

    append test of hash

---
+++ main/hash/HashEqualityProbability.java
@@ -0,0 +1,29 @@
+package main.hash;
+
+public class HashEqualityProbability {
+
+        public static void main(String[] args) {
+        
+        HashEqualityProbability hashEqualityProbability = new HashEqualityProbability();        
+        
+        int numberOfProbability = 10;
+        for (int i = 8; i > 0; i--){ numberOfProbability *= 10; }
+        int prefix = 2;
+        for (double i = 10; i > 0; i--){ prefix *= 2; }
+        
+        numberOfProbability += prefix;
+        double probability = hashEqualityProbability.getProbability(numberOfProbability);
+        System.out.println(probability);
+
+    }
+
+    public double getProbability(int number){
+        
+        System.out.println("\n" + (1.0/365));
+        double p = Math.pow((1.0/365), number);
+        for(int i = (366 - number); i < 366; i++){ p *= i; }
+        return 1 - p;
+
+    }
+
+}
---
+++ test/main/hash/HashEqualityProbabilityTest.java
@@ -0,0 +1,35 @@
+package test.main.hash;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class HashEqualityProbabilityTest {
+
+    @Test
+    public static void main(String[] args) {
+        
+        HashEqualityProbabilityTest hashEqualityProbability = new HashEqualityProbabilityTest();        
+        
+        int numberOfProbability = 10;
+        for (int i = 8; i > 0; i--){ numberOfProbability *= 10; }
+        int prefix = 2;
+        for (double i = 10; i > 0; i--){ prefix *= 2; }
+        
+        numberOfProbability += prefix;
+        System.out.println(numberOfProbability);
+        double probability = hashEqualityProbability.getProbability(numberOfProbability);
+        System.out.println(probability);
+        assertEquals((Double)1.0, (Double)probability);
+        
+    }
+
+    public double getProbability(int number){
+        
+        System.out.println("\n" + 1.0/365);
+        double p = Math.pow((1.0/365), number);
+        for(int i = (366 - number); i < 366; i++){ p *= i; }       
+        return 1 - p;
+
+    }
+
+}
