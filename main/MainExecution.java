0b1ffc1073759800fa6cff4a9998c18c02fee160
PatrickLeonardo <ticklf7@gmail.com>
2023-03-21 21:33:42 -0300

    update keypairs class

---
+++ main/MainExecution.java
@@ -0,0 +1,11 @@
+package main;
+
+import main.algorithm.keys.KeyPairs;
+
+public class MainExecution {
+    public static void main(String[] args) {
+        
+        KeyPairs.createPHAKey(4);
+
+    }
+}
