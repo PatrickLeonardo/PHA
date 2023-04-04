66096895f018fc9cf2956819a35885c9ce4126e9
PatrickLeonardo <ticklf7@gmail.com>
2023-04-03 22:28:59 -0300

    Update README.md

--- main/MainExecution.java
+++ main/MainExecution.java
@@ -6,7 +6,7 @@ import main.algorithm.utilities.bitManager;
 public class MainExecution {
     public static void main(String[] args) {
         
-        System.out.println("\n" + KeyPairs.createPHAKey(4));
+        System.out.println("\n" + KeyPairs.createPHAKey(4) + "\n");
         bitManager.byteToBit((byte) 9);
 
     }
