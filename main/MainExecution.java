b655fd24834878afecbd53a5974dfadc36d6463f
PatrickLeonardo <ticklf7@gmail.com>
2023-04-05 18:05:57 -0300

    ajust repo again

---
+++ main/MainExecution.java
@@ -0,0 +1,13 @@
+package main;
+
+import main.algorithm.keys.PHAKeyCipher;
+import main.algorithm.utilities.bitManager;
+
+public class MainExecution {
+    public static void main(String[] args) {
+        
+        System.out.println("\n" + PHAKeyCipher.createPHAKey(4) + "\n");
+        bitManager.byteToBit((byte) 9);
+
+    }
+}
