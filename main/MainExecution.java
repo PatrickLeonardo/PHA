432e4a08ab514fd0c802df0acdc9061c541f4529
patrick <patricklf7@proton.me>
2023-08-05 22:50:07 -0300

    stay message

--- main/MainExecution.java
+++ main/MainExecution.java
@@ -5,8 +5,7 @@ import main.algorithm.key.PHAKeyCipher;
 public class MainExecution {
     public static void main(String[] args) {
         
-        System.out.println("\n" + PHAKeyCipher.createPHAKey(9) + "\n");
-        bitManager.byteToBit((byte) 9);
+        PHAKeyCipher.createPHAKey(9);
 
     }
 }
