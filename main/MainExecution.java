e8324323eeb3e772031d90ae3f4f62ea3702801f
patrick <patricklf7@proton.me>
2023-08-05 22:49:44 -0300

    repo await

--- main/MainExecution.java
+++ main/MainExecution.java
@@ -4,8 +4,9 @@ import main.algorithm.key.PHAKeyCipher;
 
 public class MainExecution {
     public static void main(String[] args) {
-
-        PHAKeyCipher.createPHAKey(9);
+        
+        System.out.println("\n" + PHAKeyCipher.createPHAKey(9) + "\n");
+        bitManager.byteToBit((byte) 9);
 
     }
 }
