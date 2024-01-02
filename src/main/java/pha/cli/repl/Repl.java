de371f6d4e5351677e06b29df3eeee1052e389a5
PatrickLeonardo <patricklf7@proton.me>
2024-01-02 20:29:16 -0300

    main: update REPL

--- src/main/java/pha/cli/repl/Repl.java
+++ src/main/java/pha/cli/repl/Repl.java
@@ -3,20 +3,25 @@ package pha.cli.repl;
 public class Repl {
 
     public static void main(final String[] args) {
-         
-        System.out.println("|  Welcome to PHAShell -- Version 1.0");
+    
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
+        
+        System.out.println("|  Welcome to PHAShell -- Version 1.0 (" + System.getProperty("os.name") + ")");
         System.out.println("|  For an introduction type: /help intro\n");
-
-        while(true) {
-
+        
+        while (true) {
+            
             final String input = System.console().readLine("phashell> ");
             
-            if(input.equals("ping")) {
+            if (input.equals("ping")) {
                 System.out.println("pong");
+            } else if (input.equals("exit")) {
+                break;
             } else {
                 System.out.println(input);
             }
-
+            
         }
 
     }
